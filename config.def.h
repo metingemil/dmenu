665f9b26f2171f30776d5ccd4c4690bace4d7024
limegch <metin.gemil@gmail.com>
2021-01-11 18:16:14 +0200

    adjust fonts for colored emojis

--- config.def.h
+++ config.def.h
@@ -4,8 +4,10 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
-};
+				  "Liberation Mono:pixelsize=12:antialias=true:autohint=true"
+				, "JoyPixels:pixelsize=12:antialias=true:autohint=true"
+ 				//, "Noto Color Emoji:pixelsize=10:antialias=true:autohint=true"
+				};
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
